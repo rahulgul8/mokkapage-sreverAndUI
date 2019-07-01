f1865dc12caf43b27fa3aa152ce5db9c06b84598
rahul <rahulgul8@gmail.com>
2019-07-01 09:13:32 +0530

    refactor

--- server/functions/firestore.js
+++ server/functions/firestore.js
@@ -6,15 +6,13 @@ admin.initializeApp(functions.config().firebase);
 const db = admin.firestore();
 
 exports.getQuestions = function() {
-    console.log('I was here');
     return db.collection("/questions").get().then((querySnapshot) => {
         const stuff = [];
         querySnapshot.forEach((doc) => {
             stuff.push(doc.data());
         });
-        console.log('test');
         return stuff;
     }).catch((e)=>{
-        console.log(e);
+        return e;
     });
 }
