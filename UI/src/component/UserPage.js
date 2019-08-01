d894b23430bb7d105b1306f5f39c10efbe03e0a5
rahulgul8 <rahulgul8@gmail.com>
2019-08-01 12:32:54 +0530

    Update UserPage.js

--- UI/src/component/UserPage.js
+++ UI/src/component/UserPage.js
@@ -1,9 +1,52 @@
 import React, { Component } from 'react';
 import Page from './Page'
-
+import * as Constant from '../constants'
 export default class UserPage extends Component {
-
-  render() {
-    return <Page questions={this.props.questions} type="user" updateState={this.props.updateState} ></Page>
+  constructor() {
+    super();
+    this.state = {
+      name: '',
+      questions: []
+    };
   }
+  render() {
+    return <Page questions={this.state.questions} type="user" updateState={this.updateState} ></Page>;
+  }
+
+  navigate() {
+    this.props.history.push("/result/" + this.props.match.params.id, { response: 'response' })
+  }
+
+  componentDidMount() {
+    fetch(Constant.HOST + '/player/response/get?id=' + this.props.match.params.id)
+      .then(res => res.json())
+      .then((data) => {
+        console.log(data);
+        debugger;
+        this.setState({ questions: data.quiz, name: data.name });
+        Constant.loadImages(data.quiz);
+      })
+      .catch(console.log)
+  }
+
+  updateState = (e) => {
+    console.log(e)
+    let page = e.page;
+
+    switch (page) {
+      case 'end':
+        Constant.updateServer(Constant.HOST + '/player/result/update', 'POST', { name: localStorage.getItem("name"), id: this.props.match.params.id, score: e.score })
+          .then(resp => {
+            console.log(resp);
+            return resp.json();
+          })
+          .then((response) => {
+            console.log(response);
+            debugger
+            this.props.history.push("/result/" + this.props.match.params.id, { response: response })
+          });
+        ; break;
+      default: throw new Error();
+    }
+  };
 }
