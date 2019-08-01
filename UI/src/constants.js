41aab6e520096080776d6a51bf89eec144d72932
rahulgul8 <rahulgul8@gmail.com>
2019-08-01 12:32:18 +0530

    Create constants.js

---
+++ UI/src/constants.js
@@ -0,0 +1,36 @@
+export const HOST = "https://us-central1-mokkapage.cloudfunctions.net/app";
+
+
+export function loadImages(questions) {
+    questions.map((q) => {
+        return q.options.map(o => o.url);
+    }).reduce((a, b) => a.concat(b), []).forEach((q) => {
+        console.log(q)
+        new Image().src = q;
+    }
+    );
+}
+
+export async function updateServer(url, method, data) {
+
+    try {
+        var resp = await fetch(url, {
+            method: method,
+            body: JSON.stringify(data),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+
+        if (resp.status >= 200 && resp.status < 300) {
+            return resp;
+        } else {
+            console.log('Somthing happened wrong');
+            return false;
+        }
+    }
+    catch (e) {
+        console.log(e);
+        return false;
+    }
+}
