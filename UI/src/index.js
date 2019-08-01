aca913548c11ead33875fb3add679cc2019651ab
rahulgul8 <rahulgul8@gmail.com>
2019-08-01 12:34:53 +0530

    Update index.js

--- UI/src/index.js
+++ UI/src/index.js
@@ -2,19 +2,26 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Result from './component/Result'
+import UserPage from './component/UserPage'
 import * as serviceWorker from './serviceWorker';
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 
 // ReactDOM.render(<App />, document.getElementById('root'));
 
-ReactDOM.render(<Router>
-    <Route path="/" component={App} />
-    <Route exact path="/quiz/:id" render={(props) => <App param={props.match.params.id} />} />
-    </Router>,
+ReactDOM.render(
+    <div className="jumbotron start">
+        <BrowserRouter>
+            <Switch>
+                <Route exact path="/" component={App} />
+                <Route exact path="/result/:id" component={Result} />
+                <Route exact path="/quiz/:id" component={UserPage} />
+            </Switch>
+        </BrowserRouter></div>,
     document.getElementById('root')
 );
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+// Learn more about service workers: https://bit.ly/CRA-PWArender={(props) => <Result param={props.match.params.id}
 serviceWorker.unregister();
