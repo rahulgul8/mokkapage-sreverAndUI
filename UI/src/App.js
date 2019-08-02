484faf6d73acd0c10e88600d73bef1bfa5bc1182
rahulgul8 <rahulgul8@gmail.com>
2019-08-02 19:09:25 +0530

    Update App.js

--- UI/src/App.js
+++ UI/src/App.js
@@ -5,9 +5,8 @@ import 'bootstrap/dist/css/bootstrap.css';
 import CreatorPage from './component/CreatorPage'
 import StartPage from './component/StartPage'
 import SharePage from './component/SharePage'
-import UserPage from './component/UserPage'
-import Result from './component/Result'
-import Constant from './constants'
+import * as Constant from './constants'
+import Wait from './component/Wait'
 
 
 export default class App extends Component {
@@ -21,46 +20,27 @@ export default class App extends Component {
     };
   }
 
-  locahost = "http://localhost:5000/mokkapage/us-central1/app";
-  host = "https://us-central1-mokkapage.cloudfunctions.net/app";
 
   componentDidMount() {
-    var url = '/getquestions';
-    if (this.props.param) {
-      url = '/player/response/get?id=' + this.props.param;
-      this.setState({ page: 'user' });
-    }
-    fetch(this.host + url)
+    fetch(Constant.HOST + '/getquestions')
       .then(res => res.json())
       .then((data) => {
         console.log(data);
-        debugger;
         this.setState({ questions: data.quiz });
-        this.loadImages(data.quiz);
+        Constant.loadImages(data.quiz);
       })
       .catch(console.log)
   }
-
-  loadImages(questions) {
-    questions.map((q) => {
-      return q.options.map(o => o.url);
-    }).reduce((a, b) => a.concat(b), []).forEach((q) => {
-      console.log(q)
-      new Image().src = q;
-    }
-    );
-  }
-
 
   updateState = (e) => {
     console.log(e)
     let page = e.page;
 
     switch (page) {
-      case 'quiz': break;
       case 'start': this.setState({ page: "quiz", name: e.name }); break;
       case 'end':
-      Constant.updateServer('/player/response/add', 'POST', { name: localStorage.getItem("name"), quiz: e.selectedQuestions })
+        this.setState({ page: "wait", name: e.name })
+        Constant.updateServer(Constant.HOST + '/player/response/add', 'POST', { name: localStorage.getItem("name"), quiz: e.selectedQuestions })
           .then(resp => {
             console.log(resp);
             return resp.json();
@@ -78,22 +58,16 @@ export default class App extends Component {
 
   getPage(page) {
     switch (page) {
-      case 'start': return <StartPage name={this.state.name} updateState={this.updateState}></StartPage>;
+      case 'start': return <StartPage name={this.state.name} updateState={this.updateState} message="Enter your name and share your quiz with your friends"></StartPage>;
       case 'quiz': return <CreatorPage name={this.state.name} updateState={this.updateState} questions={this.state.questions}></CreatorPage>;
-      case 'end': return <SharePage domain={Constant.HOST + "http://oorga.co/fancywish?name="} quizId={this.getKey()} name={this.state.name} updateState={this.updateState} ></SharePage >;
-      case 'user':
-        return <UserPage questions={this.state.questions} updateState={this.updateState}></UserPage>;
-      case 'result': return <Result></Result>;
+      case 'end': return <SharePage domain={Constant.APP_HOST + "/quiz/"} quizId={this.state.quizId} name={this.state.name} updateState={this.updateState} ></SharePage >;
+      case 'wait': return <Wait></Wait>
       default: throw new Error();
     }
   }
 
-  getKey() {
-    return this.state.quizId;
-  }
-
   render() {
-    return <div className="jumbotron start">{this.getPage(this.state.page)}</div>;
+    return this.getPage(this.state.page)
   }
 
 
