a6990ed18512818e4cf5b26e7cee93d8bbe1ae38
rahulgul8 <rahulgul8@gmail.com>
2019-08-02 19:11:12 +0530

    Update Result.js

--- UI/src/component/Result.js
+++ UI/src/component/Result.js
@@ -1,15 +1,43 @@
 import React, { Component } from 'react';
-import './ResultPage.css'
+import './style/ResultPage.css';
+import * as Constant from '../constants'
 export default class Result extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      rows: []
+    };
+  }
 
   render() {
     console.log(this.props.location.state);
-    let rows = this.props.location.state.response;
+    let rows = this.state.rows;
     return this.getTable(rows);
   }
 
+  redirect() {
+    this.props.history.push('/')
+  }
+
+  componentDidMount() {
+    if (!this.props.location.state || !this.props.location.state.response) {
+      fetch(Constant.HOST + '/player/result/get?id=' + this.props.match.params.id)
+        .then(res => res.json())
+        .then((data) => {
+          data.sort((a, b) => b.score - a.score);
+          this.setState({ rows: data });
+        })
+        .catch(console.log)
+    } else {
+      this.props.location.state.response.sort((a, b) => b.score - a.score);
+      this.setState({ rows: this.props.location.state.response });
+    }
+  }
+
   getTable(rows) {
-    return (<div className="tablediv"><table className="table table-striped">
+    var count = 0;
+    return (<div><button className="btn btn-primary" onClick={this.redirect.bind(this)}>Create your own quiz-></button><div className="tablediv"><table className="table table-striped">
       <thead>
         <tr>
           <th>Name</th>
@@ -18,13 +46,13 @@ export default class Result extends Component {
       </thead>
       <tbody>
         {rows.map(row => {
-          return (<tr>
+          return (<tr key={count++}>
             <td>{row.name}</td>
             <td>{row.score}</td>
           </tr>);
         })}
       </tbody>
-    </table></div>);
+    </table></div></div>);
   }
 
 }
