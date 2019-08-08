4617571e14f272c6f0cd1907c2a23251c9efffb5
rahulgul8 <rahulgul8@gmail.com>
2019-08-08 19:37:06 +0530

    Update SharePage.js

--- UI/src/component/SharePage.js
+++ UI/src/component/SharePage.js
@@ -4,20 +4,31 @@ export default class SharePage extends Component {
 
   render() {
     var url = encodeURI(this.props.domain + this.props.quizId);
-    return (<div>
-      <div id='share' contentEditable='true' className="shareDiv badgeC badge-bottom-right" onClick={this.copy.bind(this)}>{this.props.domain + this.props.quizId}</div>
-      <div className="shareButtons">
-        <a href={'whatsapp://send?text=' + url}>
-          <img src="https://img.icons8.com/color/48/000000/whatsapp.png" alt="Whatsapp" /></a>
-        <a href={"https://www.facebook.com/sharer/sharer.php?u=" + url} target="_blank" rel="noopener noreferrer">
-          <img src="https://img.icons8.com/color/48/000000/facebook-circled.png" alt="Share on Facebook" />
-        </a>
+    var title = encodeURI(this.props.title + " ");
+    return (<div><h5>Your Quiz is now ready!! <br />Share this URL with your friends and see who knows you best.</h5>
+      <div id="shareUrl" contentEditable='true' className="shareDiv badgeC badge-bottom-right" onClick={this.copy.bind(this)}>{this.props.domain + this.props.quizId}</div>
+      <div id='share'>
+        <a className="whatsapp" href={'https://api.whatsapp.com/send?text=' + title + url}><i className="fa fa-whatsapp"></i></a>
+
+        <a className="facebook" href={"https://www.facebook.com/share.php?u=" + url + "&title=" + title} target="blank"><i className="fa fa-facebook"></i></a>
+
+        {/* <a className="facebook-messenger" href={"fb-messenger://share/?link=" + url + "&app_id=123456789"}><i className="fab fa-facebook-messenger"></i></a> */}
+
+        <a className="twitter" href={"https://twitter.com/intent/tweet?status=" + title + " +" + url} target="blank"><i className="fa fa-twitter"></i></a>
+
+        <a className="linkedin" href={"https://www.linkedin.com/shareArticle?mini=true&url=" + url + "&title=" + title} target="blank"><i className="fa fa-linkedin"></i></a>
+
+        <a className="copy" href='javascript:;' onClick={this.copy.bind(this)}><i className="fa fa-copy"></i></a>
       </div>
     </div >);
   }
 
-  copy(e) {
-    document.execCommand('selectAll');
-    document.execCommand('copy');
+  copy() {
+    var copyText = document.getElementById("shareUrl");
+    setTimeout(function () {
+      copyText.focus();
+      document.execCommand('selectAll');
+      document.execCommand("copy");
+    }, 0);
   }
 }
