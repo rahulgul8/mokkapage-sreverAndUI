ea85da792a12385c617c4a508dba6901a261ce47
rahulgul8 <rahulgul8@gmail.com>
2019-08-08 19:24:07 +0530

    Update App.js

--- UI/src/App.js
+++ UI/src/App.js
@@ -68,12 +68,14 @@ export default class App extends Component {
     switch (page) {
       case 'start': return <StartPage name={this.state.name} updateState={this.updateState} message="Enter your name to Start"></StartPage>;
       case 'quiz': return <CreatorPage name={this.state.name} updateState={this.updateState} questions={this.state.questions}></CreatorPage>;
-      case 'end': return (<div>
-        <SharePage domain={Constant.APP_HOST + "/quiz/"} quizId={this.state.quizId} name={this.state.name} updateState={this.updateState} ></SharePage >
-        <br />
-        Who knows <strong>{sessionStorage.getItem('name')}</strong> best?
+      case 'end':
+        var title = sessionStorage.getItem('name') + " has challenged you with a Friendship Dare. Try this challenge now!";
+        return (<div>
+          <SharePage domain={Constant.APP_HOST + "/quiz/"} quizId={this.state.quizId} name={this.state.name} updateState={this.updateState} title={title}></SharePage >
+          <br />
+          Who knows <strong>{sessionStorage.getItem('name')}</strong> best?
         <Result id={this.state.quizId}></Result>
-      </div>);
+        </div>);
       case 'wait': return <Wait></Wait>
       default: throw new Error();
     }
