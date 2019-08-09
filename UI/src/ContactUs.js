a42897836facc9ca7f8e7cd7fd305eab26855b32
rahulgul8 <rahulgul8@gmail.com>
2019-08-09 20:05:43 +0530

    Create ContactUs.js

---
+++ UI/src/ContactUs.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+export default class ContactUs extends Component {
+
+    render() {
+        return <div><div><a class="homeButton" href="/"><i class="fa fa-home"></i> BACK</a></div><br /> Email us your Queries, Suggestions and Ideas at <br /><strong>oorgaoffice@gmail.com</strong><br />
+        </div>
+    }
+}
