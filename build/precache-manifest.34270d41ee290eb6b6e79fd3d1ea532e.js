501d23c6dd669cb340ffe5992ec213e63cb6ded2
rahul <rahulgul8@gmail.com>
2019-08-13 08:40:41 +0530

    project restructuring with minor css fixes

---
+++ build/precache-manifest.34270d41ee290eb6b6e79fd3d1ea532e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aef69b2c40a24e702ba162727411cc7e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cc7c90cda72a341ef110",
+    "url": "/static/css/2.510409cd.chunk.css"
+  },
+  {
+    "revision": "b5dd4ebc926f12357f72",
+    "url": "/static/css/main.cb1e84ea.chunk.css"
+  },
+  {
+    "revision": "cc7c90cda72a341ef110",
+    "url": "/static/js/2.5d0df9f8.chunk.js"
+  },
+  {
+    "revision": "b5dd4ebc926f12357f72",
+    "url": "/static/js/main.71510cd4.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
