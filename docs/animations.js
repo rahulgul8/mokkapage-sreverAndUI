1d25e980b9ce62de9e583dc3cb6aef41ae6283b8
Kbl <rahulgul8@gmail.com>
2019-08-15 11:01:01 +0530

    test pages

---
+++ docs/animations.js
@@ -0,0 +1,268 @@
+isMobile = false; //initiate as false
+// device detection
+if (/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|ipad|iris|kindle|Android|Silk|lge |maemo|midp|mmp|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i.test(navigator.userAgent)
+    || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(navigator.userAgent.substr(0, 4))) {
+    isMobile = true;
+}
+
+function animate() {
+    console.log(isMobile)
+    clickBackground();
+    animateBody();
+}
+
+function animateBody() {
+
+    clickBackground();
+    const colors = ["#3CC157", "#2AA7FF", "#1B1B1B", "#FCBC0F", "#F85F36"];
+    const numBalls = isMobile ? 50 : 100;
+    const balls = [];
+
+    for (let i = 0; i < numBalls; i++) {
+        let ball = document.createElement("div");
+        ball.classList.add("ball");
+        ball.style.background = colors[Math.floor(Math.random() * colors.length)];
+        ball.style.left = `${Math.floor(Math.random() * 100)}vw`;
+        ball.style.top = `${Math.floor(Math.random() * 100)}vh`;
+        ball.style.transform = `scale(${Math.random()})`;
+        ball.style.width = `${Math.random()}em`;
+        ball.style.height = ball.style.width;
+
+        balls.push(ball);
+        document.body.append(ball);
+    }
+
+    // Keyframes
+    balls.forEach((el, i, ra) => {
+        let to = {
+            x: Math.random() * (i % 2 === 0 ? -1 : 2),
+            y: Math.random() * 1
+        };
+
+        let anim = el.animate(
+            [
+                { transform: "translate(0, 0)" },
+                { transform: `translate(${to.x}rem, ${to.y}rem)` }
+            ],
+            {
+                duration: (Math.random() + 1) * 2000, // random duration
+                direction: "alternate",
+                fill: "both",
+                iterations: Infinity,
+                easing: "ease-in-out"
+            }
+        );
+    });
+}
+
+function clickBackground() {
+    var c = document.getElementById("c");
+    var ctx = c.getContext("2d");
+    var cH;
+    var cW;
+    var bgColor = "#FF6138";
+    var animations = [];
+    var circles = [];
+
+    var colorPicker = (function () {
+        var colors = ["#FF6138", "#FFBE53", "#2980B9", "#71A503"];
+        var index = 0;
+        function next() {
+            index = index++ < colors.length - 1 ? index : 0;
+            return colors[index];
+        }
+        function current() {
+            return colors[index]
+        }
+        return {
+            next: next,
+            current: current
+        }
+    })();
+
+    function removeAnimation(animation) {
+        var index = animations.indexOf(animation);
+        if (index > -1) animations.splice(index, 1);
+    }
+
+    function calcPageFillRadius(x, y) {
+        var l = Math.max(x - 0, cW - x);
+        var h = Math.max(y - 0, cH - y);
+        return Math.sqrt(Math.pow(l, 2) + Math.pow(h, 2));
+    }
+
+    function addClickListeners() {
+        document.addEventListener("touchstart", handleEvent);
+        document.addEventListener("mousedown", handleEvent);
+    };
+
+    function handleEvent(e) {
+        if (e.touches) {
+            e.preventDefault();
+            e = e.touches[0];
+        }
+        var currentColor = colorPicker.current();
+        var nextColor = colorPicker.next();
+        var targetR = calcPageFillRadius(e.pageX, e.pageY);
+        var rippleSize = Math.min(200, (cW * .4));
+        var minCoverDuration = 750;
+
+        var pageFill = new Circle({
+            x: e.pageX,
+            y: e.pageY,
+            r: 0,
+            fill: nextColor
+        });
+        var fillAnimation = anime({
+            targets: pageFill,
+            r: targetR,
+            duration: Math.max(targetR / 2, minCoverDuration),
+            easing: "easeOutQuart",
+            complete: function () {
+                bgColor = pageFill.fill;
+                removeAnimation(fillAnimation);
+            }
+        });
+
+        var ripple = new Circle({
+            x: e.pageX,
+            y: e.pageY,
+            r: 0,
+            fill: currentColor,
+            stroke: {
+                width: 3,
+                color: currentColor
+            },
+            opacity: 1
+        });
+        var rippleAnimation = anime({
+            targets: ripple,
+            r: rippleSize,
+            opacity: 0,
+            easing: "easeOutExpo",
+            duration: 900,
+            complete: removeAnimation
+        });
+
+        var particles = [];
+        for (var i = 0; i < 32; i++) {
+            var particle = new Circle({
+                x: e.pageX,
+                y: e.pageY,
+                fill: currentColor,
+                r: anime.random(24, 48)
+            })
+            particles.push(particle);
+        }
+        var particlesAnimation = anime({
+            targets: particles,
+            x: function (particle) {
+                return particle.x + anime.random(rippleSize, -rippleSize);
+            },
+            y: function (particle) {
+                return particle.y + anime.random(rippleSize * 1.15, -rippleSize * 1.15);
+            },
+            r: 0,
+            easing: "easeOutExpo",
+            duration: anime.random(1000, 1300),
+            complete: removeAnimation
+        });
+        animations.push(fillAnimation, rippleAnimation, particlesAnimation);
+    }
+
+    function extend(a, b) {
+        for (var key in b) {
+            if (b.hasOwnProperty(key)) {
+                a[key] = b[key];
+            }
+        }
+        return a;
+    }
+
+    var Circle = function (opts) {
+        extend(this, opts);
+    }
+
+    Circle.prototype.draw = function () {
+        ctx.globalAlpha = this.opacity || 1;
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, this.r, 0, 2 * Math.PI, false);
+        if (this.stroke) {
+            ctx.strokeStyle = this.stroke.color;
+            ctx.lineWidth = this.stroke.width;
+            ctx.stroke();
+        }
+        if (this.fill) {
+            ctx.fillStyle = this.fill;
+            ctx.fill();
+        }
+        ctx.closePath();
+        ctx.globalAlpha = 1;
+    }
+
+    var animate = anime({
+        duration: Infinity,
+        update: function () {
+            ctx.fillStyle = bgColor;
+            ctx.fillRect(0, 0, cW, cH);
+            animations.forEach(function (anim) {
+                anim.animatables.forEach(function (animatable) {
+                    animatable.target.draw();
+                });
+            });
+        }
+    });
+
+    var resizeCanvas = function () {
+        var root = document.body;
+        cW = root.clientWidth;
+        cH = root.clientHeight;
+        c.width = cW * devicePixelRatio;
+        c.height = cH * devicePixelRatio;
+        ctx.scale(devicePixelRatio, devicePixelRatio);
+    };
+
+    (function init() {
+        resizeCanvas();
+        window.addEventListener("resize", resizeCanvas);
+        addClickListeners();
+        if (!!window.location.pathname.match(/fullcpgrid/)) {
+            startFauxClicking();
+        }
+        handleInactiveUser();
+    })();
+
+    function handleInactiveUser() {
+        var inactive = setTimeout(function () {
+            fauxClick(cW / 2, cH / 2);
+        }, 2000);
+
+        function clearInactiveTimeout() {
+            clearTimeout(inactive);
+            document.removeEventListener("mousedown", clearInactiveTimeout);
+            document.removeEventListener("touchstart", clearInactiveTimeout);
+        }
+
+        document.addEventListener("mousedown", clearInactiveTimeout);
+        document.addEventListener("touchstart", clearInactiveTimeout);
+    }
+
+    function startFauxClicking() {
+        setTimeout(function () {
+            fauxClick(anime.random(cW * .2, cW * .8), anime.random(cH * .2, cH * .8));
+            startFauxClicking();
+        }, anime.random(200, 900));
+    }
+
+    function fauxClick(x, y) {
+        var fauxClick = new Event("mousedown");
+        fauxClick.pageX = x;
+        fauxClick.pageY = y;
+        document.dispatchEvent(fauxClick);
+    }
+    (function ontimeout() {
+        fauxClick(anime.random(cW * .2, cW * .8), anime.random(cH * .2, cH * .8));
+        setTimeout(ontimeout, (Math.random() * 1000) + 5000);
+    })();
+
+}
---
+++ docs/precache-manifest.63471415e310139a4635db6b89c9167a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ebec296acaeb676f3c0c32726637ea8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cc7c90cda72a341ef110",
+    "url": "/static/css/2.510409cd.chunk.css"
+  },
+  {
+    "revision": "55a05972251625c83e29",
+    "url": "/static/css/main.9de692e4.chunk.css"
+  },
+  {
+    "revision": "cc7c90cda72a341ef110",
+    "url": "/static/js/2.5d0df9f8.chunk.js"
+  },
+  {
+    "revision": "55a05972251625c83e29",
+    "url": "/static/js/main.98b7457a.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
