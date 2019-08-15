5f02b1bc8d30afbc1a70b84d2c4fc4d4c9e67d6f
Kbl <rahulgul8@gmail.com>
2019-08-15 11:06:42 +0530

    test ad

--- src/ContactUs.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react';
-
-export default class ContactUs extends Component {
-
-    render() {
-        return <div><div><a className="homeButton" href="/"><i className="fa fa-home"></i> BACK</a></div><br /> Email us your Queries, Suggestions and Ideas at <br /><strong>oorgaoffice@gmail.com</strong><br />
-        </div>
-    }
-}
